926f62c21e3c2cd2fa4ee4558bcc87ad789225c4
jwmueller <jwmueller@users.noreply.github.com>
2023-02-10 00:48:38 +0000

    deploy: cleanlab/cleanlab@5f6493f5098e03460c887ab28d073f9432750686

--- versioning.js
+++ versioning.js
@@ -1,4 +1,4 @@
 var Version = {
-  version_number: "placeholder_version_number",
-  commit_hash: "e2611e76a1a11595a7b4ec84ac699e39ba1424bc",
+  version_number: "v2.2.0",
+  commit_hash: "5f6493f5098e03460c887ab28d073f9432750686",
 };
