0720724a7e48da9cb1c663d4e537019ae2b02233
ulya-tkch <ulya-tkch@users.noreply.github.com>
2026-01-08 18:32:01 +0000

    deploy: cleanlab/cleanlab@d7e4f0b3ea7bd0d2b3284d632e44110f348dd101

--- versioning.js
+++ versioning.js
@@ -1,4 +1,4 @@
 var Version = {
-  version_number: "v2.7.1",
-  commit_hash: "758c4826727f538b40925264fd5bff164e864b98",
+  version_number: "v2.8.0",
+  commit_hash: "d7e4f0b3ea7bd0d2b3284d632e44110f348dd101",
 };
